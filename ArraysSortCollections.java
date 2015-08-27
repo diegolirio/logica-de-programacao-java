726ee3b8b96cac41f8a3ba69ef06bd9b5439dd49
Diego Lirio <diegolirio.dl@gmail.com>
2015-08-27 15:00:34 -0300

    ...

--- ArraysSortCollections.java
+++ ArraysSortCollections.java
@@ -1,6 +1,7 @@
 package br.com.tdv.precoleta.model;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 
 public class ArraysSortCollections {
@@ -10,14 +11,19 @@ public class ArraysSortCollections {
 	    for(int i: arrayInt) System.out.println(i);
 	}
 
-	// TODO: 
-	public static void printSortReverse(int[] arrayInt) {
-		Arrays.sort(arrayInt);
-		for(int i: arrayInt) System.out.println(i);
+	public static void printSortReverse(int[] arrayInt) { 
+		Integer[] arr = new Integer[arrayInt.length];
+		for (int i = 0; i < arrayInt.length; i++) 
+			arr[i] = arrayInt[i];
+		Arrays.sort(arr, Collections.reverseOrder());
+	    System.out.println(Arrays.toString(arrayInt));
 	}
 	
 	public static void main(String[] args) {
 		int[] array = {9,3,5,7,3,2,1,0,-1};
 	    printSort(array);
+	    printSortReverse(array);
     }
+	
+	
 }
